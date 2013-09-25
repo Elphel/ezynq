9e2871f097fff7ed72a3f6ebd61ba3fde8b9707e
Oleg Dzhimiev <oleg@elphel.com>
2013-09-25 12:20:47 -0600

    +header

--- install_uboot.sh
+++ install_uboot.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+#***************************************************************************
+# FILE NAME  : install_uboot.sh
+# DESCRIPTION: gets xilinx's u-boot and merges with ezynq
+# AUTHOR: Oleg Dzhimiev <oleg@elphel.com>
+# Copyright (C) 2013 Elphel, Inc
+# -----------------------------------------------------------------------------**
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  The four essential freedoms with GNU GPL software:
+#  * to run the program for any purpose
+#  * to study how the program works and change it to make it do what you wish
+#  * to redistribute copies so you can help your neighbor
+#  * to distribute copies of your modified versions to others
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------**
 
 #get the current script path
 SCRIPT=$(readlink -f "$0")
